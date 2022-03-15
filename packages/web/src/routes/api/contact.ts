71ea3a5445f4804627a34bb84b36c164ab99ab79
stordahl <jacob.stordahl@stylitics.com>
2022-03-14 21:23:48 -0500

    Fixes SendGRid implementation

--- packages/web/src/routes/api/contact.ts
+++ packages/web/src/routes/api/contact.ts
@@ -3,17 +3,21 @@ import type { MessageContent } from '$lib/types'
 
 /** @type {import('@sveltejs/kit').RequestHandler} */
 
-export async function post({ body }) {
+export async function post({ request }) {
+  const body = await request.formData()
   await mail.setApiKey(process.env["SENDGRID_API_KEY"])
 
-  const address = import.meta.env.VITE_EMAIL_ADDRESS.toString()
+  const toAddress = import.meta.env.VITE_EMAIL_ADDRESS_TO.toString()
+  const fromAddress = import.meta.env.VITE_EMAIL_ADDRESS_FROM.toString()
 
   const msg:MessageContent = {
-    to: address,
-    from: body.get("email"),
-    subject: `New Contact Form Submission from ${body.get("name")}`,
+    to: toAddress,
+    from: fromAddress,
+    subject: `New Contact Form Submission from ${body.get("name")} at ${body.get("email")}`,
     text: body.get("message"),
-    html: `<p>${body.get("message")}</p>`,
+    html: `<p>New Submission from ${body.get("name")} at ${body.get("email")}</p>
+          <p>Message Subject: ${body.get("reason")}</p>
+          <p>${body.get("message")}</p>`,
   }
 
   mail
